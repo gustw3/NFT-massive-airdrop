4ee0d039a5aa2ed00ece406c8093672c36f50f71
Augustin <augustin@mbp-de-gust.home>
2022-12-31 11:30:55 +0100

    deploy frontend

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+
+const Header = () => {
+
+  return(
+    <div className="wrapper">
+      <div className="left">
+        <h2>The gate <br/> we promised <br/> is here !</h2>
+        <button><a href="https://twitter.com/augustin_br">Follow me for more trick 🐦</a></button>
+        <p>Take it easy ❤️</p>
+      </div>
+      <div className="right">
+        <img src="https://bafybeidrtmaal7xhemlmobzr6n3ctozi7zq33mmcabbeiy3q7e2sfqn23u.ipfs.nftstorage.link/images/gate.png" />
+      </div>
+
+    </div>
+  )
+}
+
+
+export default Header;
